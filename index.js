a51b4fba407f06285d73ec326f628617052d9459
Ochiengsteven <stevenochieng6305@gmail.com>
2024-04-11 14:38:42 +0300

    CONNECT TO THE DB

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const PORT = 3000;
+
+// Encode the password before inserting it into the connection string
+const password = encodeURIComponent("Ny@t1ndo");
+
+mongoose
+  .connect(
+    `mongodb+srv://admin:${password}@backenddb.p3hflbt.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB`
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Connection failed!", err);
+  });
 
 app.get('/', (req, res) => {
-  res.send('Hello from my App!')
-})
-
-app.listen(3000, () => {
-  console.log('Express app listening on port 3000!')
-})
+  res.send('Hello my App!');
+});
